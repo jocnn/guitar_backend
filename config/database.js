408641a914f3f338dfdf06fd74a0208cf16ee6b1
jocnn <ijann_@outlook.com>
2022-05-11 15:07:18 -0300

    cambiando conexion a docker local

--- config/database.js
+++ config/database.js
@@ -4,17 +4,13 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'mongoose',
       settings: {
-        host: env('DATABASE_HOST', 'cluster0.6rrk3.mongodb.net'),
-        srv: env.bool('DATABASE_SRV', true),
+        host: env('DATABASE_HOST', '0.0.0.0'),
+        srv: env.bool('DATABASE_SRV', false),
         port: env.int('DATABASE_PORT', 27017),
         database: env('DATABASE_NAME', 'guitar_la_strapi'),
         username: env('DATABASE_USERNAME', 'root'),
         password: env('DATABASE_PASSWORD', '9021090210'),
-      },
-      options: {
-        authenticationDatabase: env('AUTHENTICATION_DATABASE', 'admin'),
-        ssl: env.bool('DATABASE_SSL', true),
-      },
+      }
     },
   },
 });
